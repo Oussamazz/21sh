d6fef858e763a5f82ad034c3cb7b94c646a4e296
Youssef Abakhar <yabakhar@e1r8p1.1337.ma>
2020-12-30 19:35:56 +0100

    begin norm src

--- SRC_HEAD/src/error_handler.c
+++ SRC_HEAD/src/error_handler.c
@@ -3,25 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   error_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oelazzou <oelazzou@student.42.fr>                +#+  +:+       +#+        */
+/*   By: oelazzou <oelazzou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/24 17:51:48 by oelazzou             #+#    #+#             */
-/*   Updated: 2020/12/23 22:30:13 by oelazzou            ###   ########.fr       */
+/*   Created: 2020/10/24 17:51:48 by oelazzou          #+#    #+#             */
+/*   Updated: 2020/12/23 22:30:13 by oelazzou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "21sh.h"
 
-
-char  *err_ret(char *s, char *addrr)
+char	*err_ret(char *s, char *addrr)
 {
-  ft_putstr_fd(s, 2);
-  return (addrr);
+	ft_putstr_fd(s, 2);
+	return (addrr);
 }
 
-void  error_message(char *err_message, int flag)
+void	error_message(char *err_message, int flag)
 {
-   ft_putstr_fd(err_message, 2);
-   if (flag)
-      exit(EXIT_FAILURE);
+	ft_putstr_fd(err_message, 2);
+	if (flag)
+		exit(EXIT_FAILURE);
 }
